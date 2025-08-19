bf3fa925ca567e37937f4aba2ed1ce9722990e40
Shashank Goel <shashank.goel.1988@gmail.com>
2025-08-19 11:44:16 +0000

    creating LLM shell file

---
+++ llm.sh
@@ -0,0 +1,8 @@
+set -e
+set -x
+apt update && sudo apt install pciutils lshw
+curl -fsSL https://ollama.com/install.sh | sh
+nohup ollama serve > ollama.log 2>&1 &
+sleep 20s
+ollama pull gemma3:1b
+ollama list
