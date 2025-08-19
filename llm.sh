40a26027d81f3ac4359338ee47eca41e95f10877
shashank goel <shashank.goel.1988@gmail.com>
2025-08-19 18:48:54 +0000

    Reducing sleep time

--- llm.sh
+++ llm.sh
@@ -3,6 +3,6 @@ set -x
 apt update && sudo apt install pciutils lshw
 curl -fsSL https://ollama.com/install.sh | sh
 nohup ollama serve > ollama.log 2>&1 &
-sleep 20s
+sleep 10ss
 ollama pull gemma3:1b
 ollama list
